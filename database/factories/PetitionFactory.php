80c72f449549daba136a0d58002d195e7dcdd704
junsan <xjunsantos@gmail.com>
2026-05-02 10:51:48 +0800

    Petition model and migrations

---
+++ database/factories/PetitionFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Petition;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Petition>
+ */
+class PetitionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
